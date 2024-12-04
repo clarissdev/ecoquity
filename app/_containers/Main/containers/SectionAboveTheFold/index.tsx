1387c3487f60695be3bf07da233e881aef764ba4
tranvo <tranvo1233@gmail.com>
2024-12-04 22:13:24 +0700

    GSAP

--- app/_containers/Main/containers/SectionAboveTheFold/index.tsx
+++ app/_containers/Main/containers/SectionAboveTheFold/index.tsx
@@ -1,3 +1,4 @@
+"use client";
 import cx from "clsx";
 import Image from "next/image";
 
@@ -7,12 +8,92 @@ import styles from "./index.module.scss";
 import Button from "@/modules/app-ui/Button";
 import Flex from "@/modules/app-ui/Flex";
 
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 type Props = {
   className?: string;
   style?: React.CSSProperties;
 };
 
 export default function SectionAboveTheFold({ className, style }: Props) {
+  useGSAP(() => {
+    gsap.fromTo(
+      "#h1",
+      {
+        x: -200,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power2.inOut",
+      }
+    );
+    gsap.fromTo(
+      "#p",
+      {
+        y: -100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+    gsap.fromTo(
+      "#p",
+      {
+        y: -100,
+        opacity: 0,
+      },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 1.25,
+        ease: "power1.inOut",
+      }
+    );
+    gsap.fromTo(
+      "#img",
+      {
+        x: 0,
+        opacity: 0,
+      },
+      {
+        x: 0,
+        opacity: 1,
+        duration: 1,
+        ease: "power1.inOut",
+      }
+    );
+    gsap.fromTo(
+      "#p",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 0.75,
+        ease: "power1.inOut",
+      }
+    );
+    gsap.fromTo(
+      "#button",
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        delay: 0.75,
+        ease: "power1.inOut",
+      }
+    );
+  }, []);
+
   return (
     <section className={cx(styles.container, className)} style={style}>
       <Flex.Row flexWrap="wrap" className={styles.content}>
@@ -22,8 +103,10 @@ export default function SectionAboveTheFold({ className, style }: Props) {
           gap="24px"
           className={styles.row}
         >
-          <h1>Ecoquity: Young People Taking Action for a Sustainable Future</h1>
-          <p>
+          <h1 id="h1">
+            Ecoquity: Young People Taking Action for a Sustainable Future
+          </h1>
+          <p id="p">
             Ecoquity is more than just a club. Born out of urgency and hope
             during a record-breaking summer, Ecoquity empowers students to take
             the lead in building a sustainable future.
@@ -42,6 +125,7 @@ export default function SectionAboveTheFold({ className, style }: Props) {
             width="400"
             src={jpgFigure}
             alt="figure"
+            id="img"
           />
         </Flex.Row>
       </Flex.Row>
