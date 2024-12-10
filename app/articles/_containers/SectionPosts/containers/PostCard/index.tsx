ea13279c7fb230132710d6033969ad319e2fee62
tranvo <tranvo1233@gmail.com>
2024-12-11 02:28:43 +0700

    Fix props + fix margin

--- app/articles/_containers/SectionPosts/containers/PostCard/index.tsx
+++ app/articles/_containers/SectionPosts/containers/PostCard/index.tsx
@@ -3,8 +3,15 @@ import styles from "./index.module.scss";
 import Image from "next/image";
 import { Post } from '@/modules/business-types';
 import { useState, useEffect } from 'react';
+import cx from "clsx";
 
-export default function PostCard ({ post }: { post: Post }){
+type Props = {
+  className?: string;
+  style?: React.CSSProperties;
+  post: Post; 
+};
+
+export default function PostCard ({ className, style, post}: Props){
   const formattedDate = new Date(post.date).toLocaleDateString('en-US', {
     year: 'numeric', 
     month: 'long', 
@@ -12,50 +19,47 @@ export default function PostCard ({ post }: { post: Post }){
   });
 
   return (
-    <article className={styles.article}>
-      {/* Card image */}
-      <div className={styles.cardImg}>
-        {/* <img 
-          src={imageUrl}
-          alt ="content image"
-          className={styles.img}
-        /> */}
-        <div className={styles.gradientOverlay}>
-          <div className={styles.title_container}>
-            <h2 className={styles.title_text}>
-              {post.title.rendered}
-            </h2>
+    <section  className={cx(styles.container, className)} style={style}>
+      <article className={styles.article}>
+          {/* Card image */}
+          <div className={styles.cardImg}>
+            {/* <Img 
+              src={imageUrl}
+              alt ="content image"
+              className={styles.img}
+            /> */}
+            <div className={styles.gradientOverlay}>
+              <div className={styles.title_container}>
+                <h2 className={styles.title_text}>
+                  {post.title.rendered}
+                </h2>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-      {/* Tags */}
-      <div className={styles.padding_1}>
-        <div className={styles.tags_container}>
-          {post.tags.map((tag) => (
-            <span 
-              key = {tag}
-              className={styles.tag}
-            >
-              {tag}
-            </span>
-          ))}
-        </div>
-      </div>
-      {/* Description & time */}
-      <div className={styles.padding_2}>
-        <div 
-          className={styles.description} 
-          dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} 
-        />
-        <time className={styles.time}>{formattedDate}</time>
-        {post.categories.map((category) => (
-          <>
-            <div>{category}</div>
-          </>
-
-        ))}
-      </div>
-    </article>
+          {/* Tags */}
+          <div className={styles.padding_1}>
+            <div className={styles.tags_container}>
+              {post.tags.map((tag) => (
+                <span 
+                  key = {tag}
+                  className={styles.tag}
+                >
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+          {/* Description & time */}
+          <div className={styles.padding_2}>
+            <div 
+              className={styles.description} 
+              dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} 
+            />
+            <time className={styles.time}>{formattedDate}</time>
+          </div>
+        </article>
+    </section>
+  
   )
 }
 
