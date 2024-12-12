d624796a441b6324efa2cafb59491ae3da28def7
tranvo <tranvo1233@gmail.com>
2024-12-13 00:58:28 +0700

    Latest articles section

--- app/_containers/Main/containers/SectionArticles/index.tsx
+++ app/_containers/Main/containers/SectionArticles/index.tsx
@@ -1,1 +1,31 @@
-export default function SectionArticles() {}
+
+import cx from "clsx";
+import styles from "./index.module.scss";
+import { Post } from "@/modules/business-types";
+import CardArticle from "@/app/articles/_containers/Main/containers/CardArticle";
+
+type Props = {
+    className?: string;
+    style?: React.CSSProperties;
+    posts: Post[];
+  };
+
+export default function SectionArticles({ className, style, posts }: Props) {
+
+    return (
+        <section className={cx(styles.container, className)} style={style}>
+            <div className={styles.container}>
+                <h1>Latest Articles</h1>
+                    <div className={styles.articlesContainer}>
+                        {posts.map((post) => (
+                            <div key={post.id} className={styles.articles}>
+                                <CardArticle  post={post} />
+                            </div>
+                        ))}
+                    </div>
+
+            </div>
+        </section>
+    )
+}
+
--- app/_containers/Main/index.tsx
+++ app/_containers/Main/index.tsx
@@ -1,17 +1,36 @@
 import cx from "clsx";
 
+import { notFound } from "next/navigation";
 import SectionAboveTheFold from "./containers/SectionAboveTheFold";
 import styles from "./index.module.scss";
+import SectionArticles from "./containers/SectionArticles";
+import { httpGet$GetPosts } from "@/modules/commands/GetPosts/fetcher";
+import { intentionallyIgnoreError } from "@/modules/error/intentionallyIgnoreError";
 
 type Props = {
   className?: string;
   style?: React.CSSProperties;
 };
 
-export default function Main({ className, style }: Props) {
+const ARTICLE_CATEGORY_ID = 3;
+
+export default async function Main({ className, style }: Props) {
+  const data = await httpGet$GetPosts(`/wp-json/wp/v2/posts`, {
+    categories: ARTICLE_CATEGORY_ID,
+  }).catch(intentionallyIgnoreError);
+  if (!data) {
+    notFound();
+  }
+  // Fetch latest post
+  const sortedPosts = data.sort((a: { date: string }, b: { date: string }) => {return new Date(b.date).getTime() - new Date(a.date).getTime();
+        });
+  const three_latest = sortedPosts.slice(0, 3);
+  
   return (
-    <main className={cx(styles.container, className)} style={style}>
-      <SectionAboveTheFold />
-    </main>
+      <main className={cx(styles.container, className)} style={style}>
+        <SectionAboveTheFold />
+        <SectionArticles posts={three_latest}/>
+      </main>
+
   );
 }
