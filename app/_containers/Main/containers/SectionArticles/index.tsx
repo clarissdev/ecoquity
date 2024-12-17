abd1046bac90205af3176d23b7c1682d38dbd585
tranvo <tranvo1233@gmail.com>
2024-12-17 20:18:05 +0700

    Change max-width for latest articles sections and center the title

--- app/_containers/Main/containers/SectionArticles/index.tsx
+++ app/_containers/Main/containers/SectionArticles/index.tsx
@@ -1,31 +1,27 @@
-
 import cx from "clsx";
 import styles from "./index.module.scss";
 import { Post } from "@/modules/business-types";
 import CardArticle from "@/app/articles/_containers/Main/containers/CardArticle";
 
 type Props = {
-    className?: string;
-    style?: React.CSSProperties;
-    posts: Post[];
-  };
+  className?: string;
+  style?: React.CSSProperties;
+  posts: Post[];
+};
 
 export default function SectionArticles({ className, style, posts }: Props) {
-
-    return (
-        <section className={cx(styles.container, className)} style={style}>
-            <div className={styles.container}>
-                <h2>Latest Articles</h2>
-                    <div className={styles.articlesContainer}>
-                        {posts.map((post) => (
-                            <div key={post.id} className={styles.articles}>
-                                <CardArticle  post={post} />
-                            </div>
-                        ))}
-                    </div>
-
+  return (
+    <section className={cx(styles.container, className)} style={style}>
+      <div className={styles.container}>
+        <h2 className={styles.title}>Latest Articles</h2>
+        <div className={styles.articlesContainer}>
+          {posts.map((post) => (
+            <div key={post.id} className={styles.articles}>
+              <CardArticle post={post} />
             </div>
-        </section>
-    )
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 }
-
