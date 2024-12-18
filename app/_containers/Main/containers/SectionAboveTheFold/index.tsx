a9802a2b7ad6b802e6c82b586d5fef0a38afafb7
Thanh Dinh <dinhvanthanh0202@gmail.com>
2024-12-18 10:01:27 +0800

    fix: animation

--- app/_containers/Main/containers/SectionAboveTheFold/index.tsx
+++ app/_containers/Main/containers/SectionAboveTheFold/index.tsx
@@ -1,10 +1,10 @@
 "use client";
-import { useGSAP } from "@gsap/react";
+import { config, animated } from "@react-spring/web";
 import cx from "clsx";
-import gsap from "gsap";
 import Image from "next/image";
 
 import jpgFigure from "./assets/figure.jpg";
+import { useAppearanceSpring } from "./hooks/useAppearanceStrings";
 import styles from "./index.module.scss";
 
 import Button from "@/modules/app-ui/Button";
@@ -16,47 +16,48 @@ type Props = {
 };
 
 export default function SectionAboveTheFold({ className, style }: Props) {
-  useGSAP(() => {
-    gsap.fromTo(
-      "h1",
-      {
-        x: -25,
-        opacity: 0,
-      },
-      {
+  const appearanceSprings = {
+    h1: useAppearanceSpring({
+      from: { x: -25, opacity: 0 },
+      to: {
         x: 0,
         opacity: 1,
-        duration: 1,
-        ease: "power2.inOut",
       },
-    );
-    gsap.fromTo(
-      "p",
-      {
+      config: config.gentle,
+      delay: 125,
+    }),
+    p: useAppearanceSpring({
+      from: {
         y: -25,
         opacity: 0,
       },
-      {
+      to: {
         y: 0,
         opacity: 1,
-        duration: 1,
-        ease: "power1.inOut",
       },
-    );
-    gsap.fromTo(
-      "img",
-      {
-        x: 0,
+      config: config.gentle,
+      delay: 125,
+    }),
+    divImg: useAppearanceSpring({
+      from: {
         opacity: 0,
       },
-      {
-        x: 0,
+      to: {
         opacity: 1,
-        duration: 1,
-        ease: "power1.inOut",
       },
-    );
-  }, []);
+      config: config.gentle,
+      delay: 125,
+    }),
+    divButton: useAppearanceSpring({
+      from: {
+        opacity: 0,
+        y: 25,
+      },
+      to: { opacity: 1, y: 0 },
+      config: config.gentle,
+      delay: 500,
+    }),
+  };
 
   return (
     <section className={cx(styles.container, className)} style={style}>
@@ -67,30 +68,44 @@ export default function SectionAboveTheFold({ className, style }: Props) {
           gap="24px"
           className={styles.row}
         >
-          <h1 className="h1">
+          <animated.h1
+            ref={appearanceSprings.h1.ref}
+            style={appearanceSprings.h1.spring}
+          >
             Ecoquity: Young People Taking Action for a Sustainable Future
-          </h1>
-          <p className="p">
+          </animated.h1>
+          <animated.p
+            ref={appearanceSprings.p.ref}
+            style={appearanceSprings.p.spring}
+          >
             Ecoquity is more than just a club. Born out of urgency and hope
             during a record-breaking summer, Ecoquity empowers students to take
             the lead in building a sustainable future.
-          </p>
-          <div>
+          </animated.p>
+          <animated.div
+            ref={appearanceSprings.divButton.ref}
+            style={appearanceSprings.divButton.spring}
+          >
             <Button.Link href="/articles">Explore Our Articles</Button.Link>
-          </div>
+          </animated.div>
         </Flex.Col>
         <Flex.Row
           flex="1 1 300px"
           justifyContent="center"
           className={styles.row}
         >
-          <Image
-            className={styles.figure}
-            width="400"
-            src={jpgFigure}
-            alt="figure"
-            priority
-          />
+          <animated.div
+            ref={appearanceSprings.divImg.ref}
+            style={appearanceSprings.divImg.spring}
+          >
+            <Image
+              className={styles.figure}
+              width="400"
+              src={jpgFigure}
+              alt="figure"
+              priority
+            />
+          </animated.div>
         </Flex.Row>
       </Flex.Row>
     </section>
