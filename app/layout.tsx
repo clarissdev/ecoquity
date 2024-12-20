3abff50ecdd8984a837e2df8857c127b74bd6112
Thanh Dinh <dinhvanthanh0202@gmail.com>
2024-12-20 18:31:54 +0800

    font optimization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
-import { Roboto } from "next/font/google";
+import cx from "clsx";
+import { Fira_Code, Manrope, Roboto } from "next/font/google";
 
 // If loading a variable font, you don't need to specify the font weight
 const fontRoboto = Roboto({
@@ -8,13 +9,33 @@ const fontRoboto = Roboto({
   variable: "--font-family-roboto",
 });
 
+// If loading a variable font, you don't need to specify the font weight
+const fontManrope = Manrope({
+  subsets: ["latin", "vietnamese"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-family-manrope",
+});
+
+const fontFiraCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-family-fira-code",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={fontRoboto.variable}>
+    <html
+      lang="en"
+      className={cx(
+        fontRoboto.variable,
+        fontManrope.variable,
+        fontFiraCode.variable,
+      )}
+    >
       {children}
     </html>
   );
