d3206b78859818de0c0881013af1f8aed7b83f94
Thanh Dinh <dinhvanthanh0202@gmail.com>
2025-06-06 12:34:28 +0700

    improve section projects

--- app/[lang]/_containers/Main/index.tsx
+++ app/[lang]/_containers/Main/index.tsx
@@ -57,8 +57,8 @@ export default async function Main({
     projects.map((project) =>
       httpGet$GetFeaturedMedia(`/wp-json/wp/v2/media`, {
         id: project.featured_media,
-      }).catch(() => undefined)
-    )
+      }).catch(() => undefined),
+    ),
   ).catch(intentionallyIgnoreError);
   if (!projectsMedia) {
     notFound();
